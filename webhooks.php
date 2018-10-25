3d0aa06770a39b340764df6a1115c919747ef82f
darkchay <44471625+darkchay@users.noreply.github.com>
2018-10-25 21:12:47 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'mFaK9UfzgeuMTffQNBmvCyONPz84EMT+FiayLMp1ztXohhRhc9uCCCIMkISb31EBAeanjWVF9nNEr5yhqA983OWktFFPUCLWhwHRAB4zLNGPdAhTuziVsYE1NAI1arzDgfn6d6q4r7AoU2C2fqAI1wdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
